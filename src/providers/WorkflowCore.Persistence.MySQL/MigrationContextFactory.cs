59e6961de53588d0adf0e7cc783865dc5d7a778d
terry-delph <tezzadelph@gmail.com>
2019-10-25 12:28:56 +0100

    Added new migration for MySql - ExecutionErrors_AdditionalExceptionDetails

---
+++ src/providers/WorkflowCore.Persistence.MySQL/MigrationContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace WorkflowCore.Persistence.MySQL
+{
+    public class MigrationContextFactory : IDesignTimeDbContextFactory<MysqlContext>
+    {
+        public MysqlContext CreateDbContext(string[] args)
+        {
+            return new MysqlContext(@"Server=127.0.0.1;Port=5432;Database=workflow;User Id=postgres;Password=password;");
+        }
+    }
+}
